6ef1a7434bace1327227f8919d2d8de7dc7fc17d
illlama <beatlemaniakih@gmail.com>
2020-01-21 10:48:34 +0900

    정규과정은 끝 + hooks, TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
-import Movie from './components/Movie';
+import React from 'react';
+import { Route, HashRouter } from 'react-router-dom';
+import About from './router/About';
+import Home from './router/Home';
+import Navigation from './components/Navigation';
+import Detail from './components/Detail';
 
+type Props = {
+  id: number;
+  year: number;
+  title: string;
+  summary: string;
+  medium_cover_image: string;
+  genres: Array<string>;
+};
 const App: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await axios
-        .get('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
-        .then(res => {
-          const data = res.data.data.movies;
-          setMovies(data);
-          setIsLoading(false);
-        });
-    };
-    fetchData();
-  }, []);
-
   return (
-    <section className="container">
-      {isLoading ? (
-        <div className="loader">
-          <span className="loader__text">'List is loading...' </span>
-        </div>
-      ) : (
-        <div className="movies">
-          {movies.map(movie => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </section>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
   );
 };
 
--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Movie.css';
+
 type Props = {
   id?: number;
   key?: number;
@@ -6,24 +10,44 @@ type Props = {
   title: string;
   summary: string;
   poster: string;
-  genres: [];
+  genres: Array<string>;
 };
-const Movie: React.FC<Props> = ({ year, title, summary, poster, genres }) => {
+const Movie: React.FC<Props> = ({
+  id,
+  year,
+  title,
+  summary,
+  poster,
+  genres,
+}) => {
   return (
     <div className="movie">
-      <img src={poster} alt={title} title={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="movie__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="movie__genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie__summary">{summary}</p>
-      </div>
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres,
+          },
+        }}
+      >
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+        </div>
+      </Link>
     </div>
   );
 };
